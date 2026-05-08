b619725bfb2e9254679a18d8b1997da84fccb3a1
satan <keebin01@gmail.com>
2026-05-08 22:24:15 +0700

    Code cause backend error

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -20,18 +20,5 @@ class Event extends Model
         'category_id',
     ];
 
-    public function speaker()
-    {
-        return $this->belongsTo(Speaker::class);
-    }
 
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
